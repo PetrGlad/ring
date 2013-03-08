db6113ca03685c3edbb96f725f95d611b206e68e
Petr Gladkikh <PetrGlad@gmail.com>
2013-03-08 12:46:00 +0700

    Minor cleanup.
    Comments and indentation updated.
    Variables renamed.

--- ring-core/src/ring/middleware/not_modified.clj
+++ ring-core/src/ring/middleware/not_modified.clj
@@ -15,7 +15,6 @@
 (defn- not-modified-since? [request response]
   (let [modified-date  (date-header response "last-modified")
         modified-since (date-header request "if-modified-since")]
-    ; TODO Response headers lookup may not work since in map header is Camel-Cased and in fn call it is all lowercase
     (and modified-date
          modified-since
          (not (.before modified-since modified-date)))))
--- ring-core/test/ring/middleware/test/not_modified.clj
+++ ring-core/test/ring/middleware/test/not_modified.clj
@@ -24,24 +24,24 @@
   (with-redefs [ring.middleware.not-modified/not-modified-response #(vector %1 %2)]
     (let [req (modified-request "Sun, 23 Sep 2012 11:00:00 GMT")
           handler (handler-modified "Jan, 23 Sep 2012 11:00:00 GMT")]
-      (is (= [(handler req) req] 
+      (is (= [(handler req) req] ; Not modified since is called with expected args
              ((wrap-not-modified handler) req))))))
 
 (deftest test-not-modified-response  
   (testing "etag match"
-           (let [request-etag "match"
-                 req {:headers {"if-none-match" request-etag}}
+           (let [known-etag "known-etag"
+                 request {:headers {"if-none-match" known-etag}}
                  handler-resp #(hash-map :status 200 :headers {"etag" %} :body "")]
-             (is (= 304 (:status (not-modified-response (handler-resp request-etag) req))))
-             (is (= 200 (:status (not-modified-response (handler-resp "laskdhfskljahdflksjdhf") req))))))
+             (is (= 304 (:status (not-modified-response (handler-resp known-etag) request))))
+             (is (= 200 (:status (not-modified-response (handler-resp "unknown-etag") request))))))
   
   (testing "not modified"
-    (let [req #(hash-map :headers {"if-modified-since" %})
-          last-modified "Sun, 23 Sep 2012 11:00:00 GMT"
-          h-resp {:status 200 :headers {"last-modified" last-modified} :body ""}]
-      (is (= 304 (:status (not-modified-response h-resp (req last-modified)))))
-      (is (= 304 (:status (not-modified-response h-resp (req "Sun, 23 Sep 2012 11:52:50 GMT")))))
-      (is (= 200 (:status (not-modified-response h-resp (req "Sun, 23 Sep 2012 10:00:50 GMT")))))))
+           (let [req #(hash-map :headers {"if-modified-since" %})
+                 last-modified "Sun, 23 Sep 2012 11:00:00 GMT"
+                 h-resp {:status 200 :headers {"last-modified" last-modified} :body ""}]
+             (is (= 304 (:status (not-modified-response h-resp (req last-modified)))))
+             (is (= 304 (:status (not-modified-response h-resp (req "Sun, 23 Sep 2012 11:52:50 GMT")))))
+             (is (= 200 (:status (not-modified-response h-resp (req "Sun, 23 Sep 2012 10:00:50 GMT")))))))
   
   (testing "no modification info"
            (let [response {:status 200 :headers {} :body ""}]
